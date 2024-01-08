99cb4414004d3214236b754725ea3b6038ab304b
Akshat Jain <akshatjain042004@email.com>
2024-01-08 15:31:03 +0530

    Added Event Page

--- src/index.js
+++ src/index.js
@@ -5,3 +5,12 @@ navButton.addEventListener("click",()=>{
     navbar.classList.toggle("active");
 })
 
+// Event Page
+var counter = 1;
+setInterval(function () {
+  document.getElementById("radio" + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 2500);
